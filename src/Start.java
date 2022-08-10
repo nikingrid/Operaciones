8d3ba52f0987b5e0ea83d8110162a66cc72358e8
nikingrid <nikpam2008@hotmail.com>
2022-08-10 18:37:41 +0200

    Add clase operaciones

--- src/Start.java
+++ src/Start.java
@@ -7,26 +7,30 @@ public class Start {
 
 		Scanner add = new Scanner(System.in);
 		
-		String opcion="";
-		int resultado=0;
-		int num1=0;
-		int num2=0;
-		
+		int opcion=0;
+	
 		System.out.print("1. Sumar" + "\n");
 		System.out.print("2. Restar" + "\n");
 		System.out.print("3. Dividir" + "\n");
 		System.out.print("4. Multiplicar" + "\n");
 		System.out.print("\n" + "Elige la operación que desees realizar: ");
-		opcion = add.next();	
+		opcion = add.nextInt();	
 		
 		System.out.print("\n");
 		
-		System.out.print("Introduce el primer número:"); 
-		num1=add.nextInt();
-		
-		System.out.print("Introduce el segundo número:"); 
-		num2=add.nextInt();
-		
-
-	}
+		switch (opcion) {
+		case 1:
+			break;
+			
+		case 2:
+			break;
+			
+		case 3:
+			break;
+			
+		case 4:
+			break;
+		}
+	
 }
+	}

68dd299df58a8dedd76daa767864f86f63effd7c
nikingrid <nikpam2008@hotmail.com>
2022-08-10 14:29:09 +0200

    Add inicio proyecto operaciones

---
+++ src/Operaciones.java
@@ -0,0 +1,12 @@
+
+public class Operaciones {
+
+	//Declaración de variables
+			String opcion="";
+			int num1 = 0;
+			int num2 = 0;
+			int resultado=0;
+			
+
+
+}
---
+++ src/Start.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner add = new Scanner(System.in);
+		
+		String opcion="";
+		
+		System.out.print("A) Sumar" + "\n" + "B) Restar" + "\n" + "C) Dividir" + "\n" + "D) Multiplicar" + "\n" + "\n" + "Elige la operación que desees realizar: ");
+		opcion = add.next();
+		
+		
+	}
+
+}

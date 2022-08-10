a817441abe7748ab3ac0ff4de652ccc35020594c
nikingrid <nikpam2008@hotmail.com>
2022-08-10 18:01:43 +0200

    Borrada clase operaciones

--- src/Start.java
+++ src/Start.java
@@ -8,18 +8,27 @@ public class Start {
 		Scanner add = new Scanner(System.in);
 		
 		String opcion="";
+		String a="a";
+		String b="b";
+		String c="c";
+		String d="d";
+		int resultado=0;
 		int num1=0;
 		int num2=0;
-		int resultado=0;
 		
-		System.out.print("A) Sumar" + "\n");
-		System.out.print("B) Restar" + "\n");
-		System.out.print("C) Dividir" + "\n");
-		System.out.print("D) Multiplicar" + "\n");
+		System.out.print("A. Sumar" + "\n");
+		System.out.print("B. Restar" + "\n");
+		System.out.print("C. Dividir" + "\n");
+		System.out.print("D. Multiplicar" + "\n");
 		System.out.print("\n" + "Elige la operación que desees realizar: ");
-		opcion = add.next();
+		opcion = add.next();	
 		
+		System.out.println("Introduce el primer número:"); 
+		num1=add.nextInt();
 		
-	}
+		System.out.println("Introduce el segundo número:"); 
+		num2=add.nextInt();
+		
 
+	}
 }

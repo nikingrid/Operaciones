b4c05dced3c5b71eb009ba91e2d602596577b0e0
nikingrid <nikpam2008@hotmail.com>
2022-08-10 19:01:30 +0200

    añadido a la clase Operaciones sus atributos, getters/setters

--- src/Operaciones.java
+++ src/Operaciones.java
@@ -1,4 +1,31 @@
 
 public class Operaciones {
 
+	//Atributos
+	protected int num1;
+	protected int num2;
+	protected int resultado;
+	
+	//Getters y Setters
+	public int getNum1() {
+		return num1;
+	}
+	public void setNum1(int num1) {
+		this.num1 = num1;
+	}
+	public int getNum2() {
+		return num2;
+	}
+	public void setNum2(int num2) {
+		this.num2 = num2;
+	}
+	public int getResultado() {
+		return resultado;
+	}
+	public void setResultado(int resultado) {
+		this.resultado = resultado;
+	}
+	
+	//Métodos
+	
 }

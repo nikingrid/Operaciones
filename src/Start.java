dd6efbac965094095dfad04fdb1f6c2137341cf1
nikingrid <nikpam2008@hotmail.com>
2022-08-10 17:00:33 +0200

    Add variables

--- src/Start.java
+++ src/Start.java
@@ -8,9 +8,13 @@ public class Start {
 		Scanner add = new Scanner(System.in);
 		
 		String opcion="";
+		int num1=0;
+		int num2=0;
+		int resultado=0;
 		
 		System.out.print("A) Sumar" + "\n" + "B) Restar" + "\n" + "C) Dividir" + "\n" + "D) Multiplicar" + "\n" + "\n" + "Elige la operación que desees realizar: ");
 		opcion = add.next();
+		
 		
 		
 	}

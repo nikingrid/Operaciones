131fc57cbe5843d271a011d41f25d2b6985af085
nikingrid <nikpam2008@hotmail.com>
2022-08-10 18:55:39 +0200

    Programa operativo

--- src/Operaciones.java
+++ src/Operaciones.java
@@ -1,4 +1,42 @@
+import java.util.Scanner;
 
 public class Operaciones {
 
+	Scanner add = new Scanner(System.in);
+	
+	int num1;
+	int num2;
+	int s;
+	int r;
+	int m;
+	int d;
+	
+	public void sumar() {
+		s=num1+num2;
+		System.out.print("El resultado de la suma es: " +s);
+	}
+	
+	public void restar() {
+		r=num1-num2;
+		System.out.print("El resultado de la resta es: " + r);
+	}
+	
+	public void multiplicar() {
+		m=num1*num2;
+		System.out.print("El resultado de la multiplicación es: " + m);
+	}
+
+	public void dividir() {
+		d=num1/num2;
+		System.out.print("El resultado de la división es: " + d);
+	}
+	
+	public void datos() {
+		System.out.print("Introduce el primer nº: ");
+		num1 = add.nextInt();	
+		
+		System.out.print("Introduce el segundo nº: ");
+		num2 = add.nextInt();
+	}
+	
 }
--- src/Start.java
+++ src/Start.java
@@ -8,11 +8,13 @@ public class Start {
 		Scanner add = new Scanner(System.in);
 		
 		int opcion=0;
+		
+		Operaciones op=new Operaciones();
 	
 		System.out.print("1. Sumar" + "\n");
 		System.out.print("2. Restar" + "\n");
-		System.out.print("3. Dividir" + "\n");
-		System.out.print("4. Multiplicar" + "\n");
+		System.out.print("3. Multiplicar" + "\n");
+		System.out.print("4. Dividir" + "\n");
 		System.out.print("\n" + "Elige la operación que desees realizar: ");
 		opcion = add.nextInt();	
 		
@@ -20,15 +22,23 @@ public class Start {
 		
 		switch (opcion) {
 		case 1:
+			op.datos();
+			op.sumar();
 			break;
 			
 		case 2:
+			op.datos();
+			op.restar();
 			break;
 			
 		case 3:
+			op.datos();
+			op.multiplicar();
 			break;
 			
 		case 4:
+			op.datos();
+			op.dividir();
 			break;
 		}
 	
